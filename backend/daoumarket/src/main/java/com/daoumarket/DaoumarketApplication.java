b6c77e53520efea58bc00c7023a5bde251467c71
KSM <sminggo5@naver.com>
2020-09-23 13:55:31 +0900

    Add backend skeleton

---
+++ backend/daoumarket/src/main/java/com/daoumarket/DaoumarketApplication.java
@@ -0,0 +1,13 @@
+package com.daoumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoumarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoumarketApplication.class, args);
+	}
+
+}
---
+++ backend/daoumarket/src/test/java/com/daoumarket/DaoumarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daoumarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoumarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
